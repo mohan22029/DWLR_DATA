354ce43a44a1264e98a3116f9011ee32e6b5b03b
keerthivasanA01 <akeerthi620@gmail.com>
2025-05-11 11:56:30 +0530

    Ready for deployment

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require('./node_modules/json-server'); // relative path to json-server
+ // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
@@ -6,5 +7,4 @@ const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
-
 server.listen(port);
